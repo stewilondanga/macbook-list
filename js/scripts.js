445b8d0d7f49f95b1fdeea43e9b0e6c4bc13d29d
Stewart ilondanga <stilondanga@gmail.com>
2021-04-10 14:17:14 +0300

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,19 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$(function(){
+  $('<style id="tableHoverStyle"></style>').appendTo('body');
+  $('td:not(:first-child), th').on('mouseenter',function(e) {
+      $('#tableHoverStyle').html('td:nth-child('+($(this).index()+1)+') {color: rgba(255,255,255,.8) !important;}');
+  });
+  $('th:first-child').on('mouseenter',function(e) {
+      $('#tableHoverStyle').html('td {color: #fff !important;}');
+  });
+    $('td:first-child').on('mouseenter',function(e) {
+      $('#tableHoverStyle').html('');
+  });
+  $('tbody').on('mouseleave',function(e){
+    $('#tableHoverStyle').html('');
+  })
+});
